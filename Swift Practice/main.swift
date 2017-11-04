0e2cfbd64cdc1cea39483497832fb4837b989b1c
Jacob <jakeluvisi@gmail.com>
2017-11-04 13:53:52 -0700

    Basic datatypes

--- Swift Practice/main.swift
+++ Swift Practice/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1.
+var integer = 32
+print(integer)
+// 2.
 
+let army = "Jacob"
+print(army)
+// 3.
+
+let bool = 9000 > 7
+print(bool)
+// 4.
+
+var double = 9.81
+print(double)
+// 5.
+
+var noValue:Int
+//print(noValue)
